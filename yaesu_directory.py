8e8a5e7bac4b94b370a18b7e82bd770f1dff404b
Kurt Kochendarfer <KE7KUS@users.noreply.github.com>
2022-10-25 07:43:42 -0600

    Create yaesu_directory.py
    Initial commit with FTM-400XDR structure.

---
+++ yaesu_directory.py
@@ -0,0 +1,13 @@
+# Directory map files are simply Python nested lists which contain the hierarchical directory names for the radio SD card outlined in each radio's manual.
+
+ds = {
+"ftm400xdr" : ["FTM400D", ["BACKUP", ["CLONE", "MEMORY", "SETUP"], "GPSLOG", "PHOTO"], "GM", "PHOTO", "QSOLOG"],
+"ft2dr" : [],
+"ft3dr" : [],
+"ft5dr" : [],
+"ftm100dr" : [],
+"ft70dr" : [],
+"ftm7250dr" : [],
+"ftm3200dr" : [],
+"ftm3207dr" : [],
+}
